e88429065d0355e493c63a26c9fe84d10b0b3b28
xiniuyuanshuai <602376307@qq.com>
2022-07-13 21:58:55 +0800

    20220713

---
+++ 工具函数.js
@@ -0,0 +1,17 @@
+// 格式化时间 倒计时类
+function countDown(time) {
+    var nowTime = +new Date();
+    var inputTime = +new Date(time);
+    var times = (inputTime - nowTime) / 1000; // 13位除1000
+    var d = parseInt(times / 60 / 60 / 24);
+    d = d < 10 ? '0' + d : d;
+    var h = parseInt(times / 60 / 60 % 24);
+    h = h < 10 ? '0' + h : h;
+    var m = parseInt(times / 60 % 60);
+    m = m < 10 ? '0' + m : m;
+    var s = parseInt(times % 60);
+    s = s < 10 ? '0' + s : s;
+    var result = d + '天' + h + '时' + m + '分' + s + '秒';
+    return result
+}
+// 走马灯
